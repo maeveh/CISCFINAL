fc9559746278ef83f4208518bc96b7c547c4d5f3
maeveh <36232314+maeveh@users.noreply.github.com>
2018-05-21 22:37:57 -0400

    Update Retirement.java

--- FinalExam/src/main/java/pkgCore/Retirement.java
+++ FinalExam/src/main/java/pkgCore/Retirement.java
@@ -25,16 +25,19 @@ public class Retirement {
 	public double MonthlySavings() {
 
 		//TODO: Calculate AmountToSave
-		double pmt = 0; // <-- this should be fixed to calculate the real pmt
+		// calculate the real pmt
+		double pmt = PMT(dAnnualReturnWorking / (double) 12, iYearsToWork * 12, 0, TotalAmountToSave(), false);
 		return pmt;
 	}
 
 	public double TotalAmountToSave() {
 		
 		//TODO: Calculate the Total Amount Requried to save
-		double pv = 0;
+		// years = 12 annual - 1 year, income - monthly payment
+		double pv = PV(dAnnualReturnRetired / (double) 12 , iYearsRetired * 12, 
+				dRequiredIncome - dMonthlySSI, 0, false);
 		//	Hint: Here's how to round a number: pv = Math.round(pv * 100.0) / 100.0;
-		return pv;
+		return Math.round(pv * 100) / 100;
 	}
 
 	public static double PMT(double r, double n, double p, double f, boolean t) {
